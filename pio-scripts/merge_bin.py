5ae09de383e2c60070ed05e3cd9d750eb0eaf3d0
T-vK <T-vK@users.noreply.github.com>
2024-10-31 16:46:50 +0100

    feat: Add github actions

--- pio-scripts/merge_bin.py
+++ pio-scripts/merge_bin.py
@@ -1,10 +1,35 @@
 Import("env")
 from os.path import join
+import os
 
 def after_build(source, target, env):
+    env_name = env.get("PIOENV")
     firmware_path = join(env.subst("$BUILD_DIR"), "firmware.bin")
-    merged_firmware_path = join(env.subst("$BUILD_DIR"), "merged-firmware.bin")
-    # Command to merge binaries (modify paths and arguments as needed)
-    env.Execute("esptool.py --chip esp32 merge_bin -o {} --flash_mode dio --flash_freq 40m --flash_size 4MB 0x1000 bootloader.bin 0x8000 partitions.bin 0xe000 boot_app0.bin 0x10000 app.bin".format(merged_firmware_path))
+    bootloader_path = join(env.subst("$BUILD_DIR"), "bootloader.bin")
+    partitions_path = join(env.subst("$BUILD_DIR"), "partitions.bin")
+    merged_firmware_path = join(env.subst("$BUILD_DIR"), f"sbxm-firmware-{env_name}.bin")
+
+    if env_name is None:
+        print("Error: PIO Environment not set. Exiting build process.")
+        sys.exit(1)  # Exit with code 1 to indicate an error
+
+    print(f"Checking for files in: {join(env.subst('$BUILD_DIR'))}")
+    print(f"Firmware Path: {firmware_path}")
+    print(f"Bootloader Path: {bootloader_path}")
+    print(f"Partitions Path: {partitions_path}")
+
+    missing_files = []
+    for path in [firmware_path, bootloader_path, partitions_path]:
+        if not os.path.exists(path):
+            missing_files.append(path)
+
+    if not missing_files:
+        # Remove check for boot_app0.bin
+        env.Execute(f"esptool.py --chip esp32 merge_bin -o {merged_firmware_path} "
+                    f"--flash_mode dio --flash_freq 40m --flash_size 4MB "
+                    f"0x1000 {bootloader_path} 0x8000 {partitions_path} "
+                    f"0x10000 {firmware_path}")
+    else:
+        print(f"One or more required files for environment {env_name} are missing: {missing_files}")
 
 env.AddPostAction("buildprog", after_build)
